dbb0960f3620e174b57cc28d7ecd89552f470aca
albertost5 <sancheztorresalberto@gmail.com>
2025-02-03 12:34:20 +0100

    Added properties structure and logic to mutate signal's state

--- src/app/products/pages/signals/pages/properties-page/properties-page.component.ts
+++ src/app/products/pages/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {User} from "../../interfaces/single-user-response.interface";
 
 @Component({
     templateUrl: './properties-page.component.html',
@@ -6,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 1,
+    email: 'johndoe@test.com',
+    first_name: 'John',
+    last_name: 'Doe',
+    avatar: 'johndoe'
+  });
+
+  onFieldUpdated(field: keyof User, value: string) {
+
+    this.user.update(current => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case "first_name":
+          current.first_name = value;
+          break;
+        case "last_name":
+          current.last_name = value;
+          break;
+      }
+
+      return current;
+    })
+  }
 }
